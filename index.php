24cfbba5e72bbd5350166661679a2ae289b25071
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-10 18:23:08 +0100

    learnt the php constructor and how to use it..

--- index.php
+++ index.php
@@ -1,40 +1,69 @@
 <?php
-class Car{
+//class Car{
+//
+//
+//    // public are called access modifiers.
+//    public $color;
+//    public $make;
+//    public $model;
+//    public $engineSize;
+//
+//
+//
+//    // class method
+//    public function accelerate(){
+//
+//    }
+//
+//    public function brake() {
+//
+//    }
+//
+//    public function turn($direction){
+//
+//    }
+//}
+//
+//// Instantiating
+//$myCar = new Car();
+//$myCar->color = 'Red';
+//$myCar->make = 'Toyota';
+//$myCar->model = 'Camry';
+//$myCar->enginesize = '6.8LpQ';
+//
+//
+//$myCar->accelerate();
+//$myCar->turn('left');
+//$myCar->brake();
+
+
+class myClass{
+    public $property1;
+    public $property2;
     
-    
-    // public are called access modifiers.
-    public $color;
-    public $make;
-    public $model;
-    public $engineSize;
-    
-    
-    
-    // class method
-    public function accelerate(){
+    public function __construct(){
+        $this->property1 = 8;
+        $this->property2 = 2;
         
     }
     
-    public function brake() {
-        
+    public function myMethod() {
+        $result = $this->property1 + $this->property2;
+        echo "The value of the property 1 is" .$this->property1 . "<br>";
+        echo "The value of the property 2 is" .$this->property2 . "<br>";
+        echo "The Result of adding propertise is" . $result;
     }
-    
-    public function turn($direction){
-        
-    } 
 }
 
-// Instantiating
-$myCar = new Car();
-$myCar->color = 'Red';
-$myCar->make = 'Toyota';
-$myCar->model = 'Camry';
-$myCar->enginesize = '6.8LpQ';
+$myObject = new myClass();
+
+//$myObject->property1 = 8;
+//$myObject->property2 = 2;
+
+$myObject->myMethod();
 
 
-$myCar->accelerate();
-$myCar->turn('left');
-$myCar->brake();
+
 
 
 ?>
